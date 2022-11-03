ef6e35d6a6506a889bff5d19d7e740a249acded3
antoine.delabays <antoine.delabays@edu.hefr.ch>
2022-11-03 11:08:04 +0100

    end of tp time

--- outside_device/main.py
+++ outside_device/main.py
@@ -22,6 +22,35 @@ dev_eui = ubinascii.unhexlify('70B3D57ED0056E96')
 
 #-------------------------------------------------------------- network mgmt -------------------------------------------------------------------
 
+class Dataload:
+    def _init_(self, name, timestamp):
+        self.name = name
+        self.timestamp = timestamp
+        self.temp
+        self.hum
+        self.light
+        self.alt
+        self.press
+        self.serialized
+
+    def update(self, py):
+        si = get_temp_hum(py)
+        self.temp = si.temperature()
+        self.hum = si.humidity()
+        lt = get_light(py)
+        self.light = lt.light()
+        press, alt = get_pressure_alt(py)
+        self.press = press
+        self.alt = alt
+
+    def serialize_data(self):
+        self.serialized = bytes([self.temp,self.hum, self.press,self.light,self.alt])
+        #self.serialized = bytes([0x01])
+
+    def send(self, sock):
+        #self.serialize()
+        send_data(sock, self.serialized)
+
 # connect to lora
 def connect_lora(dev_eui, app_eui, app_key):
     lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
@@ -74,23 +103,43 @@ def receive_data():
 
 ##----------------------------------------------------------------------------- sensor fetching ---------------------------------------------
 
-def get_temp(py):
+def get_temp_hum(py):
     si = SI7006A20(py)
     print("Temperature: " + str(si.temperature())+ " deg C and Relative Humidity: " + str(si.humidity()) + " %RH")
     print("Dew point: "+ str(si.dew_point()) + " deg C")
     t_ambient = 24.4
     print("Humidity Ambient for " + str(t_ambient) + " deg C is " + str(si.humid_ambient(t_ambient)) + "%RH")
 
-    return str(si.temperature())
+    return si
+
+
+def get_light(py):
+    lt = LTR329ALS01(py)
+    print("Light (channel Blue lux, channel Red lux): " + str(lt.light()))
+    return lt
+
+def get_pressure_alt(py):
+
+    mp = MPL3115A2(py,mode=ALTITUDE) # Returns height in meters. Mode may also be set to PRESSURE, returning a value in Pascals
+    print("MPL3115A2 temperature: " + str(mp.temperature()))
+    print("Altitude: " + str(mp.altitude()))
+    mpp = MPL3115A2(py,mode=PRESSURE) # Returns pressure in Pa. Mode may also be set to ALTITUDE, returning a value in meters
+    print("Pressure: " + str(mpp.pressure()))
+    alt = mp.altitude()
+    pressure = mpp.pressure()
+
+    return alt, pressure
 
 ##------------------------------------------------------------------------------- exec -------------------------------------------------------
 
 lora = connect_lora(dev_eui, app_eui, app_key)
 sock = open_socket()
-data = bytes([0x01, 0x02, 0x03])
-send_data(sock, data)
 
 
+dt = Dataload("payload", 0)
 py = Pycoproc(Pycoproc.PYSENSE)
 
-get_temp(py)
+#while True:
+dt.update(py)
+dt.serialize_data()
+dt.send(sock)
